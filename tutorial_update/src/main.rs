2f1fd295ed716de2ab2ea57e0aacb86cd426aaa8
kotabrog <suzuki.kota0331@gmail.com>
2023-03-25 23:58:34 +0900

    split logical device

--- tutorial_update/src/main.rs
+++ tutorial_update/src/main.rs
@@ -7,7 +7,6 @@
 
 use std::mem::size_of;
 use std::ptr::copy_nonoverlapping as memcpy;
-use std::collections::HashSet;
 
 use std::time::Instant;
 use std::fs::File;
@@ -32,9 +31,11 @@ use winit::window::{Window, WindowBuilder};
 
 mod instance;
 mod physical_device;
+mod logical_device;
 
 use instance::create_instance;
 use physical_device::pick_physical_device;
+use logical_device::create_logical_device;
 
 const VALIDATION_ENABLED: bool =
     cfg!(debug_assertions);
@@ -523,61 +524,9 @@ pub struct AppData {
 }
 
 //================================================
-// Physical Device
-//================================================
-
-//================================================
 // Logical Device
 //================================================
 
-unsafe fn create_logical_device(
-    instance: &Instance,
-    data: &mut AppData,
-) -> Result<Device> {
-    let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
-
-    let mut unique_indices = HashSet::new();
-    unique_indices.insert(indices.graphics);
-    unique_indices.insert(indices.present);
-
-    let queue_priorities = &[1.0];
-    let queue_infos = unique_indices
-        .iter()
-        .map(|i| {
-            vk::DeviceQueueCreateInfo::builder()
-                .queue_family_index(*i)
-                .queue_priorities(queue_priorities)
-        })
-        .collect::<Vec<_>>();
-
-    let layers = if VALIDATION_ENABLED {
-        vec![VALIDATION_LAYER.as_ptr()]
-    } else {
-        vec![]
-    };
-
-    let extensions = DEVICE_EXTENSIONS
-        .iter()
-        .map(|n| n.as_ptr())
-        .collect::<Vec<_>>();
-
-    let features = vk::PhysicalDeviceFeatures::builder()
-        .sampler_anisotropy(true)
-        .sample_rate_shading(true);
-
-    let info = vk::DeviceCreateInfo::builder()
-        .queue_create_infos(&queue_infos)
-        .enabled_layer_names(&layers)
-        .enabled_extension_names(&extensions)
-        .enabled_features(&features);
-
-    let device = instance.create_device(data.physical_device, &info, None)?;
-
-    data.graphics_queue = device.get_device_queue(indices.graphics, 0);
-    data.present_queue = device.get_device_queue(indices.present, 0);
-
-    Ok(device)
-}
 
 //================================================
 // Swapchain
