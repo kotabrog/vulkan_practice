1fae7d383c74f1f630ffd0ab9731fa905e91aecb
kotabrog <suzuki.kota0331@gmail.com>
2023-03-26 00:37:59 +0900

    split descriptor

--- tutorial_update/src/main.rs
+++ tutorial_update/src/main.rs
@@ -39,6 +39,7 @@ mod depth_object;
 mod texture;
 mod model;
 mod buffers;
+mod descriptor;
 
 use instance::create_instance;
 use physical_device::pick_physical_device;
@@ -52,6 +53,7 @@ use depth_object::create_depth_objects;
 use texture::{create_texture_image, create_texture_image_view, create_texture_sampler};
 use model::load_model;
 use buffers::{create_vertex_buffer, create_index_buffer, create_uniform_buffers};
+use descriptor::{create_descriptor_pool, create_descriptor_sets};
 
 const VALIDATION_ENABLED: bool =
     cfg!(debug_assertions);
@@ -540,76 +542,8 @@ pub struct AppData {
 }
 
 //================================================
-// Buffers
-//================================================
-
-
-//================================================
 // Descriptors
 //================================================
-
-unsafe fn create_descriptor_pool(device: &Device, data: &mut AppData) -> Result<()> {
-    let ubo_size = vk::DescriptorPoolSize::builder()
-        .type_(vk::DescriptorType::UNIFORM_BUFFER)
-        .descriptor_count(data.swapchain_images.len() as u32);
-
-    let sampler_size = vk::DescriptorPoolSize::builder()
-        .type_(vk::DescriptorType::COMBINED_IMAGE_SAMPLER)
-        .descriptor_count(data.swapchain_images.len() as u32);
-
-    let pool_sizes = &[ubo_size, sampler_size];
-    let info = vk::DescriptorPoolCreateInfo::builder()
-        .pool_sizes(pool_sizes)
-        .max_sets(data.swapchain_images.len() as u32);
-
-    data.descriptor_pool = device.create_descriptor_pool(&info, None)?;
-
-    Ok(())
-}
-
-unsafe fn create_descriptor_sets(device: &Device, data: &mut AppData) -> Result<()> {
-    let layouts = vec![data.descriptor_set_layout; data.swapchain_images.len()];
-    let info = vk::DescriptorSetAllocateInfo::builder()
-        .descriptor_pool(data.descriptor_pool)
-        .set_layouts(&layouts);
-
-    data.descriptor_sets = device.allocate_descriptor_sets(&info)?;
-
-    for i in 0..data.swapchain_images.len() {
-        let info = vk::DescriptorBufferInfo::builder()
-            .buffer(data.uniform_buffers[i])
-            .offset(0)
-            .range(size_of::<UniformBufferObject>() as u64);
-
-        let buffer_info = &[info];
-        let ubo_write = vk::WriteDescriptorSet::builder()
-            .dst_set(data.descriptor_sets[i])
-            .dst_binding(0)
-            .dst_array_element(0)
-            .descriptor_type(vk::DescriptorType::UNIFORM_BUFFER)
-            .buffer_info(buffer_info);
-
-        let info = vk::DescriptorImageInfo::builder()
-            .image_layout(vk::ImageLayout::SHADER_READ_ONLY_OPTIMAL)
-            .image_view(data.texture_image_view)
-            .sampler(data.texture_sampler);
-        
-        let image_info = &[info];
-        let sampler_write = vk::WriteDescriptorSet::builder()
-            .dst_set(data.descriptor_sets[i])
-            .dst_binding(1)
-            .dst_array_element(0)
-            .descriptor_type(vk::DescriptorType::COMBINED_IMAGE_SAMPLER)
-            .image_info(image_info);
-
-        device.update_descriptor_sets(
-            &[ubo_write, sampler_write],
-            &[] as &[vk::CopyDescriptorSet],
-        );
-    }
-
-    Ok(())
-}
 
 //================================================
 // Command Buffers
